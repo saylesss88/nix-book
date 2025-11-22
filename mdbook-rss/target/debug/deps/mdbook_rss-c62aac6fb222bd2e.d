9101e226928ab8f3193493772e2fda0805d59623
saylesss88 <saylesss87@proton.me>
2025-11-22 15:27:59 -0500

    Missing Chapters

---
+++ mdbook-rss/target/debug/deps/mdbook_rss-c62aac6fb222bd2e.d
@@ -0,0 +1,9 @@
+/home/jr/nix-book/mdbook-rss/target/debug/deps/mdbook_rss-c62aac6fb222bd2e.d: src/lib.rs Cargo.toml
+
+/home/jr/nix-book/mdbook-rss/target/debug/deps/libmdbook_rss-c62aac6fb222bd2e.rmeta: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
